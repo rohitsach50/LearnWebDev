4cafbdc84d5680e9689badceeb2b8cec2a28b6d2
rohit kumar <rohitsach50@gmail.com>
2022-03-02 19:24:37 +0530

    Update index.js

--- AirQualityMapIndia/src/index.js
+++ AirQualityMapIndia/src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 // var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
 ReactDOM.render(<App />, document.getElementById("root"))
-// mapboxgl.accessToken = 'pk.eyJ1Ijoicm9oaXRzYWNoNTAiLCJhIjoiY2t3a2V6NzBkMWZ1dzJvcGJuZTdyenR4eiJ9.s3NJeSr4h_roznn00RcAdA';
+// mapboxgl.accessToken = 'tokenhere';
 // var map = new mapboxgl.Map({
 //     container: 'map-el',
 //     style: 'mapbox://styles/rohitsach50/ckwkg30zm8rdv14oc4tmffjv0'
